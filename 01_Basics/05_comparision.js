4b278c0cac1dcd6a82a3c6ec64a9455ae73c738c
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-10-17 03:06:55 +0000

    Added 05_comparision.js

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,12 @@
+ console.log("2">1);
+ console.log("02">1);
+
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);
+ 
+
+ //comparision and equality check works Differently.
+ // comparision will convert null to 0 but equality will not.
+ 
+ 
