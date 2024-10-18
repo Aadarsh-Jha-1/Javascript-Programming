df608effb3f70f300b16707b9fe2e7ae490096cc
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-10-18 03:14:56 +0000

    Added Datatypes_Summary.js

---
+++ 01_Basics/06_Datatypes_Summary.js
@@ -0,0 +1,25 @@
+// primitive
+
+// 7 Types - String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+// Array - 
+
+const heros =["A","B","C"]; 
+
+// object
+
+let myObj = {
+    name : "abc",
+    roll : "xyz",
+}
+
+// function
+const func = function(){
+    console.log("Hello World");
+}
