7aa069e7d3a3505250ba5eea268062c1b2f1299b
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-10-19 05:10:01 +0000

    Added stack and heap

---
+++ 01_Basics/07_MemoryAllocation.js
@@ -0,0 +1,12 @@
+/*  
+ 2 Types Of Memories -> 1. Stack 2. Heap
+
+ => Stack stores primitive data types.
+ => In stack we get a copy of variable and not the actual variable.
+ 
+ => Heap stores non- primitive data types.
+ => In heap we get the reference of original variable.
+
+
+*/
+
