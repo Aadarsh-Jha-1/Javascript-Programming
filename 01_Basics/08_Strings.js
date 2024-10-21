9da47b1aae3e8a3575a6b2f68a7e8d909f65b726
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-10-21 07:15:42 +0000

    Added 08_Strings.js

---
+++ 01_Basics/08_Strings.js
@@ -0,0 +1,47 @@
+const name = "Aadarsh";
+const repoCount = 4;
+
+console.log(name + repoCount + "Value");
+
+// string interpolation
+
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}.`);
+
+// string declearation
+
+ const gameName = new String('xyuzhgfhgfhgfhgf');
+
+ console.log(gameName[0]);
+ console.log(gameName[1]);
+ console.log(gameName[2]);
+ console.log(gameName[3]);
+
+ console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+
+ console.log(gameName.toUpperCase());
+
+ console.log(gameName.charAt(1));
+
+ console.log(gameName.indexOf('u'));
+
+ console.log(gameName.substring(2,5));//last value not included.
+
+ console.log(gameName.slice(2,5));// same as substring but we can put negative value (which will count from left to right) .
+
+ const newstr = "   add  ";
+
+ console.log(newstr);
+
+ console.log(newstr.trim());// clears the white spaces and new  lines.
+
+ console.log(gameName.replace('xyuzhg','******'));
+ 
+ 
+
+ 
+ 
+
+
+ 
